6bc9b9453f21b754944238e517ede0af61456009
jdera.dev <138844491+jdera@users.noreply.github.com>
2025-02-26 14:03:09 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'OpenBooks',
+  tagline: 'Stories of Impossible Things',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -57,9 +57,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'OpenBooks',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'OpenBooks Logo',
           src: 'img/logo.svg',
         },
         items: [
