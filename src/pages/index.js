5c5beb1c5b2cb14f4121e5ca7ccb4c0abeee290c
jdera.dev <138844491+jdera@users.noreply.github.com>
2025-02-28 16:42:41 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Start Reading 📖🔉
+            Start Reading 📖
           </Link>
         </div>
       </div>
