cbd5aa88a8be757ab32620ed7ba9fb2d82f81196
jdera.dev <138844491+jdera@users.noreply.github.com>
2025-02-28 17:24:10 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula'); // You can
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'OPENBOOK',
-  tagline: 'Stories of Impossible Things',
+  tagline: 'STORIES OF IMPOSSIBLE THINGS',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
